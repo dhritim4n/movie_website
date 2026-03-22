4ac14b170326538d79be2e7b46e12a85e6c2d8b3
Lenovo <Lenovo@WIN11>
2026-03-22 16:18:01 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.248'],
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/movie/discover',
-        permanent: true
-      }
-    ]
-  }
+  allowedDevOrigins: ['192.168.1.248']
 
 };
 
--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+export const dynamic = 'force-dynamic';
 import React from "react"
 import { useParams } from "next/navigation"
 import { UseMovieDetails, UseMovieTrailer } from "@/hooks/movies"
