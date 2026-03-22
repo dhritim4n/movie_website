09eec5e2f2f8bc1eb74910819ebee23dbd3fa0a2
Lenovo <Lenovo@WIN11>
2026-03-22 16:09:59 +0530

    fix route issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.248']
+  allowedDevOrigins: ['192.168.1.248'],
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/movie/discover',
+        permanent: true
+      }
+    ]
+  }
 
 };
 
